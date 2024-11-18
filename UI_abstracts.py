64aec0b8980697e4db51451d280df2bb48b8c393
Игорь <iantuganov@yandex.ru>
2024-11-19 02:31:50 +0500

    UI_abstracts added, there is Draggable and Resizable classes

---
+++ UI_abstracts.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+import pygame
+from constants import SCREEN_RECT, BUTTON_SCREEN_COLLISION_DEFLATION
+
+
+class Draggable(ABC):
+    def __init__(self, position, size):
+        self.rect = pygame.Rect(position, size)
+        self.dragging = False
+        self.dragging_start_mouse = None
+        self.dragging_start_top_left = None
+
+    def handle_dragging(self, mouse_position):
+        if not self.dragging:
+            self.dragging = True
+            self.dragging_start_mouse = mouse_position
+            self.dragging_start_top_left = self.rect.topleft
+        else:
+            last_position = self.rect.topleft
+            offset_x = mouse_position[0] - self.dragging_start_mouse[0]
+            offset_y = mouse_position[1] - self.dragging_start_mouse[1]
+            self.rect.x = self.dragging_start_top_left[0] + offset_x
+            self.rect.y = self.dragging_start_top_left[1] + offset_y
+            if not SCREEN_RECT.contains(self.rect.inflate(*BUTTON_SCREEN_COLLISION_DEFLATION)):
+                self.dragging = False
+                self.rect.topleft = last_position
+
+
+class Resizable(ABC):
+    @abstractmethod
+    def get_rect(self) -> pygame.Rect:
+        pass
+
+    @abstractmethod
+    def set_size(self, size: tuple[int, int]):
+        pass
+
+    @abstractmethod
+    def get_size(self) -> tuple[int, int]:
+        pass
+
+    @abstractmethod
+    def recreate_sprites_after_resizing(self):
+        pass
+
+    def handle_size_changing(self, ctrl_alt_shift_array, mouse_wheel_state):
+        sign = mouse_wheel_state.value
+        if ctrl_alt_shift_array[2]:
+            value = sign * 5
+        else:
+            value = sign
+
+        curr_size = self.get_size()
+        new_width, new_height = curr_size
+        if ctrl_alt_shift_array[0]:
+            new_width += value
+        if ctrl_alt_shift_array[1]:
+            new_height += value
+
+        self.set_size((new_width, new_height))
+        self.recreate_sprites_after_resizing()
--- buttons.py
+++ buttons.py
@@ -1,46 +1,48 @@
 import pygame
 from states import ButtonState, MouseWheelState
-from abc import ABC
 from commands import TextCommand
 import json
 import os
-from constants import *
+from constants import BUTTON_COLOR_DICT2
 from fonts import fonts_dict
+from UI_abstracts import Draggable, Resizable
 
 
-class Button(ABC):
+class Button(Draggable, Resizable):
+    def set_size(self, size: tuple[int, int]):
+        self.rect.size = size
+
+    def get_size(self) -> tuple[int, int]:
+        return self.rect.size
+
+    def recreate_sprites_after_resizing(self):
+        self.create_all_sprites()
+
+    def get_rect(self) -> pygame.Rect:
+        return self.rect
+
     def __init__(self, text, command: TextCommand, position, size,
                  colors=BUTTON_COLOR_DICT2,
                  font_key=None,
-                 radius=7,):
-        self.position = position
-        self.dragging = False
-        self.dragging_start_mouse = None
-        self.dragging_start_top_left = None
-
+                 radius=7):
+        Draggable.__init__(self, position, size)
         self.text = text
-        self.size = size
-        self.rect = pygame.Rect(position, size)
-
         self.command = command
-
         self.font_key = font_key
         assert font_key in fonts_dict
         self.font = fonts_dict[font_key]
-
         self.current_state = ButtonState.NORMAL
-
         self.radius = radius
-        self.colors = colors  # Словарь с ключами: ButtonState.NORMAL, ButtonState.HOVER, ButtonState.ACTIVE
+        self.colors = colors
         self.sprites = {}
         self.create_all_sprites()
 
     @classmethod
     def from_dict(cls, kwargs):
-        return Button(kwargs['text'],
-                      TextCommand(kwargs['command']),
-                      kwargs['position'],
-                      kwargs['size'])
+        return cls(kwargs['text'],
+                   TextCommand(kwargs['command']),
+                   kwargs['position'],
+                   kwargs['size'])
 
     def create_all_sprites(self):
         self.sprites = {
@@ -50,7 +52,7 @@ class Button(ABC):
         }
 
     def create_sprite(self, color):
-        sprite = pygame.Surface(self.size, pygame.SRCALPHA)
+        sprite = pygame.Surface(self.rect.size, pygame.SRCALPHA)
         pygame.draw.rect(sprite, color, sprite.get_rect(), border_radius=self.radius)
         text_surface = self.font.render(self.text, True, (0, 0, 0))
         text_rect = text_surface.get_rect(center=sprite.get_rect().center)
@@ -86,36 +88,6 @@ class Button(ABC):
 
         if unpressed:
             return self.command
-
-    def handle_dragging(self, mouse_position):
-        if not self.dragging:
-            self.dragging = True
-            self.dragging_start_mouse = mouse_position
-            self.dragging_start_top_left = self.rect.topleft
-        else:
-            last_position = self.rect.topleft
-            offset_x = mouse_position[0] - self.dragging_start_mouse[0]
-            offset_y = mouse_position[1] - self.dragging_start_mouse[1]
-            self.rect.x = self.dragging_start_top_left[0] + offset_x
-            self.rect.y = self.dragging_start_top_left[1] + offset_y
-            if not SCREEN_RECT.contains(self.rect.inflate(*BUTTON_SCREEN_COLLISION_DEFLATION)):
-                self.dragging = False
-                self.rect.topleft = last_position
-
-    def handle_size_changing(self, ctrl_alt_shift_array, mouse_wheel_state):
-        sign = mouse_wheel_state.value
-        if ctrl_alt_shift_array[2]:
-            value = sign * 5
-        else:
-            value = sign
-
-        if ctrl_alt_shift_array[0]:
-            self.size = self.size[0] + value, self.size[1]
-        if ctrl_alt_shift_array[1]:
-            self.size = self.size[0], self.size[1] + value
-
-        self.create_all_sprites()
-        self.rect.size = self.size
 
 
 def load_button_positions(file_path: str):
